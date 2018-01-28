55a0055a930808eb3da8d4f3bde768313e8751a1
Gordon Brander <gordonbrander@users.noreply.github.com>
2018-01-28 10:57:06 -0800

    Add tests for doc loading

---
+++ test/package_data/fixtures/Doc with meta.md
@@ -0,0 +1,6 @@
+---
+title: "Doc title"
+date: 2018-01-28
+summary: "The summary"
+---
+Lorem ipsum
---
+++ test/test_doc.py
@@ -0,0 +1,48 @@
+"""
+Unit tests for Doc
+"""
+
+import unittest
+from pathlib import Path
+from lettersmith import doc as Doc
+
+module_path = Path(__file__).parent
+fixtures_path = Path(module_path, "package_data", "fixtures")
+
+class test_bare_doc(unittest.TestCase):
+    def setUp(self):
+        doc_path = fixtures_path.joinpath("Bare doc.md")
+        doc = Doc.load(doc_path, relative_to=fixtures_path)
+        self.doc = doc
+
+    def test_content(self):
+        self.assertEqual(self.doc["content"], "Lorem ipsum")
+
+    def test_meta(self):
+        """
+        Meta should always be a dict
+        """
+        self.assertEqual(type(self.doc["meta"]), dict)
+
+    def test_simple_path(self):
+        self.assertEqual(str(self.doc["simple_path"]), "Bare doc.md")
+
+
+class test_doc_with_meta(unittest.TestCase):
+    def setUp(self):
+        doc_path = fixtures_path.joinpath("Doc with meta.md")
+        doc = Doc.load(doc_path, relative_to=fixtures_path)
+        self.doc = doc
+
+    def test_content(self):
+        self.assertEqual(self.doc["content"], "Lorem ipsum")
+
+    def test_meta(self):
+        """
+        Meta should always be a dict
+        """
+        self.assertEqual(self.doc["meta"]["title"], "Doc title")
+
+
+if __name__ == '__main__':
+    unittest.main()
