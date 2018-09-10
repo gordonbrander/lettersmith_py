232a67aa96099b83e9a998adf7130cbf8ef1fdd6
Gordon Brander <gordonbrander@users.noreply.github.com>
2018-09-09 18:08:06 -0700

    Throw more useful errors from yamltools

--- lettersmith/yamltools.py
+++ lettersmith/yamltools.py
@@ -1,4 +1,25 @@
-from yaml import load as loads_yaml
+import yaml
+import frontmatter
+
+
+ParserError = yaml.parser.ParserError
+ScannerError = yaml.scanner.ScannerError
+
+
+def load_frontmatter(pathlike):
+    with open(str(pathlike)) as f:
+        try:
+            meta, content = frontmatter.parse(f.read())
+            return meta, content
+        # Raise a more descriptive errors
+        except ParserError as error:
+            raise ParserError(
+                'Could not parse YAML from frontmatter in "{}"'.format(pathlike)
+            ) from error
+        except ScannerError as error:
+            raise ScannerError(
+                'Error scanning "{}"'.format(pathlike)
+            ) from error
 
 
 def load(file_path):
@@ -7,8 +28,8 @@ def load(file_path):
     python dict. Returns a python dict.
     """
     with open(file_path, "r") as f:
-        return loads_yaml(f.read())
+        return yaml.load(f.read())
     return {}
 
 
-loads = loads_yaml
+loads = yaml.load
