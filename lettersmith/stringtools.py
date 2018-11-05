705abd2e72c92cc471ab4c1b0bf2d5a5ea90078a
Gordon Brander <gordonbrander@users.noreply.github.com>
2018-11-04 20:29:48 -0800

    Simplify truncate logic and add tests

--- lettersmith/stringtools.py
+++ lettersmith/stringtools.py
@@ -11,10 +11,9 @@ def truncate(text, max_len=250, suffix="..."):
     Truncate a text string to a certain number of characters,
     trimming to the nearest word boundary.
     """
-    trimmed = text.strip()
-    if len(trimmed) < max_len:
-        return trimmed
-    elif re.match("\s", trimmed[max_len]):
-        return re.sub("\s+$", "", trimmed[0:max_len]) + suffix
-    else:
-        return re.sub("\s+\S+?$", "", trimmed[0:max_len]) + suffix
+    stripped = strip_html(text).strip()
+    if len(stripped) <= max_len:
+        return stripped
+    substr = stripped[0:max_len + 1]
+    words = " ".join(re.split(r"\s+", substr)[0:-1])
+    return words + suffix
--- test/test_stringtools.py
+++ test/test_stringtools.py
@@ -27,5 +27,55 @@ class test_strip_html(unittest.TestCase):
         s = stringtools.strip_html(html)
         self.assertEqual(s, '')
 
+
+class test_truncate(unittest.TestCase):
+    def test_text(self):
+        s = """
+Shall I compare thee to a summer’s day?
+Thou art more lovely and more temperate.
+Rough winds do shake the darling buds of May,
+And summer’s lease hath all too short a date.
+Sometime too hot the eye of heaven shines,
+And often is his gold complexion dimmed;
+And every fair from fair sometime declines,
+By chance, or nature’s changing course, untrimmed;
+But thy eternal summer shall not fade,
+Nor lose possession of that fair thou ow’st,
+Nor shall death brag thou wand’rest in his shade,
+When in eternal lines to Time thou grow’st.
+So long as men can breathe, or eyes can see,
+So long lives this, and this gives life to thee.
+        """
+        truncated = stringtools.truncate(s, max_len=50)
+        self.assertEqual(
+            truncated,
+            "Shall I compare thee to a summer’s day? Thou art..."
+        )
+
+    def test_html(self):
+        s = """
+<p>
+<i>Shall I compare thee to a summer’s day?</i><br>
+Thou art more lovely and more temperate.<br>
+Rough winds do shake the darling buds of May,<br>
+And summer’s lease hath all too short a date.<br>
+Sometime too hot the eye of heaven shines,<br>
+And often is his gold complexion dimmed;<br>
+And every fair from fair sometime declines,<br>
+By chance, or nature’s changing course, untrimmed;<br>
+But thy eternal summer shall not fade,<br>
+Nor lose possession of that fair thou ow’st,<br>
+Nor shall death brag thou wand’rest in his shade,<br>
+When in eternal lines to Time thou grow’st.<br>
+So long as men can breathe, or eyes can see,<br>
+So long lives this, and this gives life to thee.
+</p>
+        """
+        truncated = stringtools.truncate(s, max_len=50)
+        self.assertEqual(
+            truncated,
+            "Shall I compare thee to a summer’s day? Thou art..."
+        )
+
 if __name__ == '__main__':
     unittest.main()
