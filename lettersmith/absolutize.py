6fd3effda9148d4fd1c5f448c89a6b3e252dfce9
Gordon Brander <gordonbrander@users.noreply.github.com>
2019-03-20 20:29:17 -0700

    Change absolutize to an iterator transformer

--- lettersmith/absolutize.py
+++ lettersmith/absolutize.py
@@ -9,30 +9,27 @@ from lettersmith import path as pathtools
 URL_ATTR = r"""(src|href)=["'](.*?)["']"""
 
 
-def absolutize(base_url="/"):
+def absolutize(docs, base_url="/"):
     """
     Absolutize URLs in content. Replaces any relative URLs in content
     that start with `/` and instead starts them with `base_url`.
 
     URLS are found by matching against `href=` and `src=`.
     """
+    # Early return if base URL is just "/"
+    if str(base_url) == "/":
+        return docs
+
     def render_inner_match(match):
         attr = match.group(1)
         value = match.group(2)
         url = pathtools.qualify_url(value, base_url)
         return '{attr}="{url}"'.format(attr=attr, url=url)
 
-    def absolutize_doc_urls(doc):
-        # Early return if base URL is just "/"
-        if str(base_url) == "/":
-            return doc
-
+    for doc in docs:
         content = re.sub(
             URL_ATTR,
             render_inner_match,
             doc.content
         )
-
-        return replace(doc, content=content)
-
-    return absolutize_doc_urls
+        yield doc._replace(content=content)
