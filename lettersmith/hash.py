d3d2a702036789ef2dffb3bcc8e81bbe83d2af0c
Gordon Brander <gordonbrander@users.noreply.github.com>
2019-05-04 10:49:48 -0700

    Remove hash utilities
    We no longer use them

--- lettersmith/hash.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def hash_digest(s, length=16):
-    """
-    Given string s, return a hexdigest of `length` (in bits).
-    """
-    h = hashlib.shake_256(s.encode("utf-8"))
-    return h.hexdigest(length)
