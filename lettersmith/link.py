8048e8c633c3e8077c2e2c182a0d66115850a5ca
Gordon Brander <gordonbrander@users.noreply.github.com>
2019-11-16 17:56:11 -0800

    Update Link to add summary
    Add from_doc() helper function for creating them

--- lettersmith/link.py
+++ lettersmith/link.py
@@ -5,11 +5,27 @@ from lettersmith.util import get
 from collections import namedtuple
 
 
-Link = namedtuple("Link", ("id_path", "output_path", "title"))
+Link = namedtuple("Link", (
+    "id_path",
+    "output_path",
+    "title",
+    "summary"
+))
 Link.__doc__ = """
 A namedtuple for representing a link entry. Just the basic information
 needed to construct a hyperlink.
 """
+
+def from_doc(doc):
+    """
+    Read link from doc
+    """
+    return Link(
+        doc.id_path,
+        doc.output_path,
+        doc.title,
+        doc.meta.get("summary", "")
+    )
 
 
 @get.register(Link)
