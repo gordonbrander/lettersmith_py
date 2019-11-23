5f85eb9a6e4e7e9d09f01f4197bbe06fdfb1e775
Gordon Brander <gordonbrander@users.noreply.github.com>
2019-11-23 13:39:26 -0800

    Simplify markdown plugin using docs.renderer

--- lettersmith/markdowntools.py
+++ lettersmith/markdowntools.py
@@ -1,44 +1,16 @@
-from pathlib import PurePath
 from markdown import markdown as md
 from mdx_gfm import GithubFlavoredMarkdownExtension
 from lettersmith.html import strip_html
-from lettersmith.doc import annotate_exceptions
+from lettersmith import docs as Docs
+from lettersmith.func import compose
 
 
 def markdown(s):
     """
-    Just a wrapper for our house flavor of markdown.
-    We use Github-flavored markdown as a base.
+    Render markdown on content field.
     """
     return md(s, extensions=(GithubFlavoredMarkdownExtension(),))
 
 
-def strip_markdown(s):
-    """
-    Strip markdown, returning a bare string. This is useful for
-    generating summaries of markdown docs.
-    """
-    return strip_html(markdown(s))
-
-
-@annotate_exceptions
-def render_doc(doc):
-    """
-    Render markdown in content field of doc dictionary.
-    Updates the output path to .html.
-    Returns a new doc.
-    """
-    content = markdown(doc.content)
-    output_path = PurePath(doc.output_path).with_suffix(".html")
-    return doc._replace(
-        content=content,
-        output_path=str(output_path)
-    )
-
-
-def content(docs):
-    """
-    Markdown rendering plugin
-    """
-    for doc in docs:
-        yield render_doc(doc)
+strip_markdown = compose(strip_html, markdown)
+content = Docs.renderer(markdown)
