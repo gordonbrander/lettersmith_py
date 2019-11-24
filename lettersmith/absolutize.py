55d55760fc316e3b269dbe21bfae9f59b2c07555
Gordon Brander <gordonbrander@users.noreply.github.com>
2019-11-23 21:52:49 -0800

    Use renderer for absolutize

--- lettersmith/absolutize.py
+++ lettersmith/absolutize.py
@@ -2,6 +2,7 @@
 Tools for making relative URLs absolute in doc content.
 """
 import re
+from lettersmith.docs import renderer
 from lettersmith.func import composable
 from lettersmith import path as pathtools
 
@@ -9,28 +10,27 @@ from lettersmith import path as pathtools
 URL_ATTR = r"""(src|href)=["'](.*?)["']"""
 
 
-@composable
-def absolutize(docs, base_url="/"):
+def absolutize(base_url):
     """
     Absolutize URLs in content. Replaces any relative URLs in content
     that start with `/` and instead starts them with `base_url`.
 
     URLS are found by matching against `href=` and `src=`.
     """
-    # Early return if base URL is just "/"
-    if str(base_url) == "/":
-        return docs
-
     def render_inner_match(match):
         attr = match.group(1)
         value = match.group(2)
         url = pathtools.qualify_url(value, base_url)
         return '{attr}="{url}"'.format(attr=attr, url=url)
 
-    for doc in docs:
-        content = re.sub(
+    @renderer
+    def render(content):
+        """
+        Absolutize URLs in doc content fields.
+        """
+        return re.sub(
             URL_ATTR,
             render_inner_match,
-            doc.content
+            content
         )
-        yield doc._replace(content=content)
+    return render
