f730e1317242f6096e18f95f2b55115f173583c2
Gordon Brander <gordonbrander@users.noreply.github.com>
2019-11-23 21:04:55 -0800

    Add archive plugin

---
+++ lettersmith/archive.py
@@ -0,0 +1,28 @@
+"""
+Generate an archive page.
+"""
+from lettersmith import doc as Doc
+from lettersmith import stub as Stub
+from lettersmith.func import composable
+
+
+@composable
+def archive(
+    docs,
+    output_path,
+    title="Archive",
+    template="archive.html"
+):
+    """
+    Generate an archive doc for a list of docs.
+    """
+    archive = tuple(Stub.stubs(docs))
+    return Doc.doc(
+        id_path=output_path,
+        output_path=output_path,
+        title=title,
+        content="",
+        section="archive",
+        template=template,
+        meta={"archive": archive}
+    )
