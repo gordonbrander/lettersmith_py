40986c995994a0bf9d4b04f7d80c023b8d47e0d8
Gordon Brander <gordonbrander@users.noreply.github.com>
2019-11-24 14:53:37 -0800

    Add meta_taxonomy lens factory

--- lettersmith/taxonomy.py
+++ lettersmith/taxonomy.py
@@ -7,7 +7,23 @@ from lettersmith import path as pathtools
 from lettersmith import stub as Stub
 from lettersmith import doc as Doc
 from lettersmith import docs as Docs
-from lettersmith import lens
+from lettersmith.lens import lens_compose, key, get, put
+
+
+_empty = tuple()
+
+
+meta_related = lens_compose(Doc.meta, key("related", _empty))
+
+
+def meta_taxonomy(tax):
+    """
+    Create a lens for taxonomy `tax`.
+    """
+    return lens_compose(Doc.meta, key(tax, _empty))
+
+
+meta_tags = meta_taxonomy("tags")
 
 
 @composable
@@ -76,33 +92,27 @@ def index_taxonomy(docs, key):
 index_tags = index_taxonomy("tags")
 
 
-_empty = tuple()
-
-
-meta_related = lens.compose(Doc.meta, lens.key("related", _empty))
-
-
-def related(taxonomy):
+def related(tax):
     """
     Annotate doc meta with a list of related doc stubs.
 
     A doc is related if it shares any of the same tags in the
     same taxonomy.
     """
-    meta_taxonomy = lens.compose(Doc.meta, lens.key(taxonomy, _empty))
-    build_index = index_taxonomy(taxonomy)
+    taxonomy = meta_taxonomy(tax)
+    build_index = index_taxonomy(tax)
     def add_related(docs):
         docs = tuple(docs)
         index = build_index(docs)
         for doc in docs:
-            tags = lens.get(meta_taxonomy, doc)
+            tags = get(taxonomy, doc)
             related = pipe(
                 _get_indexes(index, tags),
                 Docs.dedupe,
                 Docs.remove_id_path(doc.id_path),
                 tuple
             )
-            yield lens.put(meta_related, doc, related)
+            yield put(meta_related, doc, related)
     return add_related
 
 
