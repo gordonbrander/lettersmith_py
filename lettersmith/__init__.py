ff8e585d7fcd540a9512d9d1aebf8138b5c140ab
Gordon Brander <gordonbrander@users.noreply.github.com>
2019-11-24 16:04:47 -0800

    Export stub and query

--- lettersmith/__init__.py
+++ lettersmith/__init__.py
@@ -7,8 +7,10 @@ from lettersmith import html
 from lettersmith import jinjatools
 from lettersmith import markdowntools
 from lettersmith import permalink
+from lettersmith import query
 from lettersmith import rss
 from lettersmith import sitemap
+from lettersmith import stub
 from lettersmith import taxonomy
 from lettersmith import wikidoc
 from lettersmith.func import rest, pipe, compose, thrush
