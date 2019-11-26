2e78fe32449cef3c7a495cbe8e81fcb0610b3508
Gordon Brander <gordonbrander@users.noreply.github.com>
2019-11-25 21:22:57 -0800

    Improved docstrings

--- lettersmith/blog.py
+++ lettersmith/blog.py
@@ -11,6 +11,13 @@ from lettersmith import docs as Docs
 def markdown_doc(base_url):
     """
     Handle typical transformations for a generic markdown doc.
+
+    - Markdown
+    - Wikilinks
+    - Transclusions
+    - Absolutizes post links
+    - Changes file extension to .html
+    - Sets template in prep for Jinja rendering later
     """
     return compose(
         absolutize.absolutize(base_url),
@@ -24,11 +31,13 @@ def markdown_page(base_url, relative_to="."):
     """
     Performs typical transformations for a page.
 
+    - Sets nice page permalink
     - Markdown
+    - Wikilinks
+    - Transclusions
     - Absolutizes post links
-    - Changes file extensions
-    - Sets nice permalink
-    - Adds templates in prep for Jinja rendering later
+    - Changes file extension to .html
+    - Sets template in prep for Jinja rendering later
     """
     return compose(
         markdown_doc(base_url),
@@ -40,12 +49,13 @@ def markdown_post(base_url):
     """
     Performs typical transformations for a blog post.
 
+    - Sets nice date-based permalink
     - Markdown
     - Wikilinks
+    - Transclusions
     - Absolutizes post links
-    - Changes file extensions
-    - Sets date-based permalink
-    - Adds templates in prep for Jinja rendering later
+    - Changes file extension to .html
+    - Sets template in prep for Jinja rendering later
     """
     return compose(
         markdown_doc(base_url),
@@ -56,6 +66,13 @@ def markdown_post(base_url):
 def html_doc(base_url):
     """
     Handle typical transformations for a generic html doc.
+
+    - Wrap non-HTML lines with paragraph tags.
+    - Wikilinks
+    - Transclusions
+    - Absolutizes post links
+    - Changes file extension to .html
+    - Sets template in prep for Jinja rendering later
     """
     return compose(
         absolutize.absolutize(base_url),
@@ -69,12 +86,13 @@ def html_page(base_url, relative_to="."):
     """
     Performs typical transformations for a page.
 
+    - Sets nice page permalink
     - Wrap non-HTML lines with paragraph tags.
     - Wikilinks
+    - Transclusions
     - Absolutizes post links
-    - Changes file extensions
-    - Sets nice permalink
-    - Adds templates in prep for Jinja rendering later
+    - Changes file extension to .html
+    - Sets template in prep for Jinja rendering later
     """
     return compose(
         html_doc(base_url),
@@ -86,12 +104,13 @@ def html_post(base_url):
     """
     Performs typical transformations for a blog post.
 
+    - Sets nice date-based permalink
     - Wrap non-HTML lines with paragraph tags.
     - Wikilinks
+    - Transclusions
     - Absolutizes post links
-    - Changes file extensions
-    - Sets date-based permalink
-    - Adds templates in prep for Jinja rendering later
+    - Changes file extension to .html
+    - Sets template in prep for Jinja rendering later
     """
     return compose(
         html_doc(base_url),
